94eb2c00efff51cfaa6c2f2e2c47fa42c45b6734
Rijvy <rrijvy@gmail.com>
2020-03-02 03:32:06 +0600

    Add project files.

---
+++ LinkedList/ILinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public interface ILinkedList<T> where T : class
+    {
+        int Size();
+        void AddFirst(T value);
+        void AddLast(T value);
+        int DeleteFirst(T value);
+        int DeleteLast(T value);
+        bool Contains(T value);
+        int IndexOf(T value);
+        T[] ToArray(T value);
+    }
+}
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class LinkedList<T> : ILinkedList<T> where T : class
+    {
+        private class Node
+        {
+            public T value;
+            public Node next;
+
+            public Node(T value)
+            {
+                this.value = value;
+            }
+        }
+
+        private Node first;
+        private Node last;
+        private int size = 0;
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void AddFirst(T value)
+        {
+            var node = new Node(value);
+
+            if (first == null)
+            {
+                first = last = node;
+
+                size++;
+            }
+            else
+            {
+                node.next = first;
+                first = node;
+
+                size++;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            var node = new Node(value);
+
+            if (first == null)
+            {
+                first = last = node;
+
+                size++;
+            }
+            else
+            {
+                last.next = node;
+                last = node;
+
+                size++;
+            }
+        }
+
+        public int DeleteFirst(T value)
+        {
+            if (first == null)
+                return -1;
+            else
+            {
+                var deletedNode = first;
+                first = deletedNode.next;
+                deletedNode.next = null;
+
+                size--;
+
+                return 1;
+            }
+        }
+
+        public int DeleteLast(T value)
+        {
+            if (first == null)
+                return -1;
+            else
+            {
+                var currentNode = first;
+
+                while (currentNode.next != last)
+                {
+                    currentNode = currentNode.next;
+                }
+
+                currentNode.next = null;
+
+                last = currentNode;
+
+                size--;
+
+                return 1;
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            if (first == null)
+                return false;
+            else
+            {
+                var current = first;
+
+                while (current.value != value)
+                {
+                    current = current.next;
+
+                    if (current == null)
+                        return false;
+                }
+
+                if (current.value == value)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public int IndexOf(T value)
+        {
+            int index = 0;
+
+            if (first == null)
+                return -1;
+            else
+            {
+                var current = first;
+
+                while (current != null)
+                {
+                    if (current.value == value)
+                        return index;
+
+                    current = current.next;
+
+                    index++;
+                }
+
+                return -1;
+            }
+        }
+
+        public T[] ToArray(T value)
+        {
+            T[] array = new T[size];
+
+            var current = first;
+
+            var index = 0;
+
+            while (current != null)
+            {
+                array[index] = current.value;
+
+                current = current.next;
+
+                index++;
+            }
+
+            return array;
+        }
+    }
+}
